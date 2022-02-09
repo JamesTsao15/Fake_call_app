e756029d75f91d68c7f4e8224c97f3313ae6c527
JamesTsao15 <jamestsao@gmail.com>
2022-02-09 11:29:16 +0800

    更新至儲存caller資料至sharepreference

--- app/src/main/java/com/example/fake_call2/Caller_Activity.kt
+++ app/src/main/java/com/example/fake_call2/Caller_Activity.kt
@@ -3,6 +3,7 @@ package com.example.fake_call2
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -11,6 +12,7 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.DocumentsContract
 import android.provider.MediaStore
+import android.telecom.Call
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -19,7 +21,11 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.io.File
+import java.lang.reflect.Type
+
 data class Caller(val name:String,val phoneNumber:String,val photoPath:String)
 class Caller_Activity : AppCompatActivity() {
     private lateinit var imgview_callerphoto:ImageView
@@ -27,12 +33,35 @@ class Caller_Activity : AppCompatActivity() {
     private lateinit var btn_Save_Caller_Information:Button
     private lateinit var editText_caller_name:EditText
     private lateinit var editText_caller_phone_number:EditText
-    val Caller_information=ArrayList<Caller>()
+    var Caller_information=ArrayList<Caller>()
     var photoPath_Store=""
     fun pick_photo(){
         val uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         val intent = Intent(Intent.ACTION_PICK, uri)
         startActivityForResult(intent,1)
+    }
+    fun save_caller_data(){
+        val sharedPreferences:SharedPreferences=
+            getSharedPreferences("caller_information", MODE_PRIVATE)
+        val editor:SharedPreferences.Editor=sharedPreferences.edit()
+        val gson:Gson=Gson()
+        var json:String=gson.toJson(Caller_information)
+        editor.putString("caller list",json)
+        editor.apply()
+    }
+    fun load_caller_data():ArrayList<Caller>{
+        val sharedPreferences:SharedPreferences=
+            getSharedPreferences("caller_information", MODE_PRIVATE)
+        val gson:Gson=Gson()
+        val json: String? =sharedPreferences.getString("caller list",null)
+        val listType=object:TypeToken<ArrayList<Caller>>(){}.type
+        val jsonArray=gson.fromJson<ArrayList<Caller>>(json,listType)
+        if(jsonArray==null){
+            return ArrayList<Caller>()
+        }
+        else{
+            return jsonArray
+        }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +71,9 @@ class Caller_Activity : AppCompatActivity() {
         btn_Save_Caller_Information=findViewById(R.id.button_Save)
         editText_caller_name=findViewById(R.id.editTextPersonName)
         editText_caller_phone_number=findViewById(R.id.editTextPhone)
+        if(load_caller_data().isEmpty()!=true){
+            Caller_information=load_caller_data()
+        }
     }
     override fun onResume() {
         super.onResume()
@@ -63,18 +95,18 @@ class Caller_Activity : AppCompatActivity() {
             Caller_information.add(newCaller)
             editText_caller_phone_number.setText("")
             editText_caller_name.setText("")
-            Log.e("JAMES",Caller_information.toString())
+//            Log.e("JAMES",Caller_information.toString())
+            save_caller_data()
         }
     }
     @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode==1 && resultCode==Activity.RESULT_OK){
-            Log.e("JAMES","照片選擇成功")
             val uri=data!!.data
             val uriPathHelper=URIPathHelper()
             val photoPath= uri?.let { uriPathHelper.getPath(this, it) }
-            Log.e("JAMES",photoPath.toString())
+//            Log.e("JAMES",photoPath.toString())
             if (photoPath != null) {
                 photoPath_Store=photoPath
             }
