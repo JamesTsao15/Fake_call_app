a4708b5e204cf8561ddbc6549770bfb34dff1d23
JamesTsao15 <jamestsao@gmail.com>
2022-02-16 21:32:14 +0800

    更新至可以切換caller(名字而已)

--- app/src/main/java/com/example/fake_call2/Choose_Caller_Activity.kt
+++ app/src/main/java/com/example/fake_call2/Choose_Caller_Activity.kt
@@ -4,19 +4,23 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.lang.IndexOutOfBoundsException
 
 class Choose_Caller_Activity : AppCompatActivity() {
     private lateinit var tv_choose_caller_Name: TextView
     private lateinit var imageView_choose_caller_photo:ImageView
     private lateinit var btn_choose_caller_last_photo:Button
     private lateinit var btn_choose_caller_next_photo:Button
+    var pick_caller_thread:Thread=Thread()
+    var leave_Activity:Boolean=false
     var callerinformation_array:ArrayList<Caller> = ArrayList<Caller>()
-
     fun load_caller_data():ArrayList<Caller>{
         val sharedPreferences: SharedPreferences =
             getSharedPreferences("caller_information", MODE_PRIVATE)
@@ -44,6 +48,51 @@ class Choose_Caller_Activity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        
+        var index=0
+        tv_choose_caller_Name.text=callerinformation_array[index].name
+        pick_caller_thread=Thread(Runnable {
+            while(leave_Activity==false){
+                runOnUiThread{
+                    if(index==0){
+                        btn_choose_caller_last_photo.visibility=View.INVISIBLE
+                        btn_choose_caller_next_photo.visibility=View.VISIBLE
+                    }
+                    else if (index==callerinformation_array.size-1){
+                        btn_choose_caller_next_photo.visibility=View.INVISIBLE
+                        btn_choose_caller_last_photo.visibility=View.VISIBLE
+                    }
+                    else{
+                        btn_choose_caller_last_photo.visibility=View.VISIBLE
+                        btn_choose_caller_next_photo.visibility=View.VISIBLE
+                    }
+                    Log.e("JAMES",index.toString())
+                }
+                Thread.sleep(200)
+            }
+        })
+
+        pick_caller_thread.start()
+        if(leave_Activity==true)pick_caller_thread.interrupt()
+        btn_choose_caller_next_photo.setOnClickListener {
+            if (index < callerinformation_array.size - 1) {
+                index += 1
+                tv_choose_caller_Name.text = callerinformation_array[index].name
+            }
+        }
+        btn_choose_caller_last_photo.setOnClickListener {
+            if (index>0){
+                index-=1
+                tv_choose_caller_Name.text=callerinformation_array[index].name
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        leave_Activity=true
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("JAMES","on Destroy")
     }
 }
